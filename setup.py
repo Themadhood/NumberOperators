c36cfc8aa05eaa284607584a99340cb3a09d0555
Themadhood's Codes <148127209+Themadhood@users.noreply.github.com>
2024-03-26 14:39:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,14 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='TPC_Error',
+    name='TPC_NumberOperators',
     version="1.0.0",#__version__,
 
-    url='https://github.com/Themadhood/Error',
+    url='https://github.com/Themadhood/NumberOperators',
     author='Themadhood Pequot',
     author_email='themadhoodpequot@gmail.com',
 
     packages=find_packages(),
-    #py_modules=['Error'],
-
-    install_requires=[
-        "gspread",#manipulates google sheets
-        "oauth2client",#logs in to google
-        "google-api-python-client",],
 
     classifiers=[
         'Intended Audience :: Developers',
